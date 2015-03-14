b7ce3b2031e1d66fd96e90a59d3cf1031b94f27b
Marco Godínez <markotom@gmail.com>
2015-03-14 14:29:48 -0600

    Added swig as template engine and main bower components

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 var config  = require('./config'),
     express = require('express'),
+    cons    = require('consolidate'),
     server  = express();
+
+// Set template engine
+server.engine('html', cons.swig);
+server.set('view engine', 'html');
+server.set('views', __dirname + '/views');
 
 // Static files
 server.use(express.static(__dirname + '/public'));
+
+// Set public bower components
+server.use('/components', express.static(__dirname + '/bower_components'));
 
 // List Routes
 require('./routes/list')(server);
